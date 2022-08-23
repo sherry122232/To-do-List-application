6ae4e6d4a9c44433182b6b404f6c1b2be0998ad9
Shuyang Wang <45084681+sherry122232@users.noreply.github.com>
2022-08-22 20:31:44 -0400

    Version-1 To-do list web application

---
+++ date.js
@@ -0,0 +1,28 @@
+// jshint esversion:6
+// console.log(module);
+module.exports.getDate=getDate;
+
+function getDate() {
+  const today = new Date();
+
+  const options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+
+  let day = today.toLocaleDateString("en-US",options);
+  return day;
+}
+
+module.exports.getDay=getDay;
+function getDay() {
+  const today = new Date();
+
+  const options = {
+    weekday:"long",
+  };
+
+  let day = today.toLocaleDateString("en-US",options);
+  return day;
+}
