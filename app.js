e8731a4e77f2365c70828d369edd7a77acfc5844
Shuyang Wang <45084681+sherry122232@users.noreply.github.com>
2022-09-12 20:53:21 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date= require(__dirname + "/date.js");
-
+const _ = require("lodash");
+const mongoose =require("mongoose");
 // console.log(date());
 
 const app = express();
@@ -15,9 +16,67 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true})); //用于解析post request信息
 app.use(express.static("public"));
 
+
+//updated 9/4
+// mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true}); //this is for local
+mongoose.connect("mongodb+srv://wang3s4:Sherry!1998@cluster0.bw3mj4n.mongodb.net/todolistDB",{useNewUrlParser: true});
+
+const itemSchema ={
+  name: String,
+};
+const Item=mongoose.model("Item",itemSchema);
+
+const listSchema = {
+  name:String,
+  items:[itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item ({
+  name:"Do homework",
+});
+const item2 = new Item ({
+  name:"Do housework",
+});
+const item3 = new Item ({
+  name:"Relax yourself",
+});
+const defaultItems=[item1,item2,item3];
+
+
+
+
 app.get("/",function(req,res){
-  let day = date.getDate();
-  // let day = date.getDay();
+  let day = date.getDay();
+
+
+//updated 9/4
+Item.find(function(err,foundItems){
+  if (foundItems.length ===0){
+
+    Item.insertMany(defaultItems, function(err){
+      if (err){
+        console.log(err);
+      } else {
+        console.log("Successfully saved all the items to itemsDB");
+      }
+    });
+    res.redirect("/");
+    //这一句话就是重新回到app.get然后回到下面那个else，因为这时候已经insertmany了
+
+    //updated 9/4
+  } else {
+    res.render("list",{listTitle:day,newListItem: foundItems});
+  }
+
+});
+});
+//updated 9/4
+
+
+
+  let day = date.getDay();
 
 
   // var currentDay=today.getDay();
@@ -25,26 +84,80 @@ app.get("/",function(req,res){
   // var day=dayName[currentDay];
     // res.sendFile(__dirname+"/index.html");
 
+
       //list 是list.ejs的name
-      res.render("list", {listTitle: day, newListItem: items});
-})
 
+//updated 9/4
+      // res.render("list", {listTitle: day, newListItem: items});
+      //updated 9/4
 
-app.post("/", function(req, res) {
-  let item = req.body.newItem;
-  console.log(req.body);
-  if (req.body.list === "Work"){  //注意这个list是按钮的名字
-      workItems.push(item);
-      res.redirect("/work");
-  }
-  else{items.push(item);
-  res.redirect("/");}
+app.get("/:custonListName", function(req,res){
+  const custonListName= _.capitalize(req.params.custonListName);
 
-  // console.log(newItem)
-
+  List.findOne({name:custonListName}, function(err,foundList){
+    if (!err){
+      if (!foundList){
+        // Create a new list
+        const list = new List({
+          name: custonListName,
+          items: defaultItems
+        });
+          list.save();
+          res.redirect("/"+custonListName);
+      } else {
+        //show an existing list
+        res.render("list",{listTitle: foundList.name, newListItem: foundList.items});
+        // console.log("Exists!");
+      }
+    }
+  });
 });
 
 
+app.post("/", function(req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName
+  });
+
+  console.log(listName);
+  if (listName ===day){
+    item.save();
+    res.redirect("/");
+  } else {
+
+    List.findOne({name: listName}, function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/"+ listName);
+    });
+  }
+});
+
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName ===day){
+    Item.findByIdAndRemove(checkedItemId,function(err){
+      if (!err){
+        console.log("Successfully deleted that item!");
+        res.redirect("/");
+      }
+    });
+  } else {
+
+    List.findOneAndUpdate({name: listName},{$pull:{items:{_id:checkedItemId}}},function(err,foundList){
+      if (!err){
+        res.redirect("/"+ listName);
+      }
+    });
+  }
+
+
+
+});
 
 app.get("/work",function(req,res){
   res.render("list",{listTitle: "Work List", newListItem: workItems});
@@ -62,7 +175,11 @@ app.get("/about",function(req,res){
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
 
-app.listen(3000,function(){
-  console.log("Server started on port 3000!")
-})
+app.listen(port,function(){
+  console.log("Server has started!")
+});
